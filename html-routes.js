3af168e5191e9036dcbfd342418e663aa311dd64
John Manouse <johnmanouse@Macintosh-2.local>
2020-08-17 06:53:54 -0400

    route files

---
+++ html-routes.js
@@ -0,0 +1,7 @@
+//Require express, path modules
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
--- node server.js
+++ node server.js
@@ -1,15 +1,30 @@
+//Require express, path modules
+
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+//Defined Port
+const port = process.env.port || 8080;
 
-app.get('/', (req, res) =>{
+//Routes
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
+
+
+app.get('/', (request, res) =>{
 
     res.json("It's working")
 });
 
 
-
+// Log started server
 app.listen(8080, () => {
     console.log("App is running at port ", 8080);
-})
+});
